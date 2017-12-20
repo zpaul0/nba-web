f27abcb91e3d5f81f4bd511df9951610312a7ef3
zpaul0 <z_paul@sina.cn>
2017-12-19 20:54:00 -0700

    add nba web project

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TopNavBar extends React.Component {
+    render() {
+        return(
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title"> Welcome to React</h1>
+            </header>
+        );
+    }
+}
